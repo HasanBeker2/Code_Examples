a5dce7a2b82198bedc560a78720832ef3031de69
hasanbeker2 <hasanbeker@gmail.com>
2023-10-03 12:44:11 +0200

    yeni

--- python_codes.py
+++ python_codes.py
@@ -1,13 +1,22 @@
-def greet(name):
-    return "Hello, " + name
+
+
+my_list = []
+
+# Function to add an element to the list
+def add_element(data_structure, element):
+    data_structure.append(element)
 
 
 
+# Function to remove an element from the list
+def remove_element(data_structure, element):
+    if element in data_structure:
+        data_structure.remove(element)
+    else:
+        print(f"{element} not found in the list.")
 
-for _ in range(3):
-    print(greet("Alice"))
-    
 
-
-
-
+# Add elements to the list using the add_element function
+add_element(my_list, 42)
+add_element(my_list, 17)
+add_element(my_list, 99)
