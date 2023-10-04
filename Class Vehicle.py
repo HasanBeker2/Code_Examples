9ea66d71d44ffa98538780e4203370e1a32e3e01
hasanbeker2 <hasanbeker@gmail.com>
2023-10-04 13:41:09 +0200

    class vehicle

--- Class Vehicle.py
+++ Class Vehicle.py
@@ -1,4 +1,17 @@
-Class Vehicle:
+class Vehicle:
+    color = "white"
+
     def __init__(self, max_speed, mileage):
         self.max_speed = max_speed
         self.mileage = mileage
+
+    def assign_seating_capacity(self,seating_capacity):
+        self.seating_capacity = seating_capacity
+
+    def display_properties(self):
+        print("Properties of the Vehicle:")
+        print("Color:", self.color)
+        print("Maximum Speed:", self.max_speed)
+        print("Mileage:", self.mileage)
+        print("Seating Capacity:", self.seating_capacity)
+       
