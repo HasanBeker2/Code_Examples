4237674faf23acd83f782b015be3bd296d42daa1
hasanbeker2 <hasanbeker@gmail.com>
2023-10-04 13:18:54 +0200

    Class Circle and draw circle with matplotlib

---
+++ Circle Class.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+
+class Circle(object):
+    
+    # Constructor
+    def __init__(self, radius=3, color='blue'):
+        self.radius = radius
+        self.color = color 
+    
+    # Method
+    def add_radius(self, r):
+        self.radius = self.radius + r
+        return(self.radius)
+    
+    # Method to draw the circle
+    def drawCircle(self):
+        plt.gca().add_patch(plt.Circle((0, 0), radius=self.radius, fc=self.color))
+        plt.axis('scaled')
+        plt.show()
+
+# Create an object of Circle class
+circle1 = Circle(10, 'red')
+
+# Draw the circle
+circle1.drawCircle()
